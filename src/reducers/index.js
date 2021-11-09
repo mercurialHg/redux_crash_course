3fd7bf8d45d5102361e4db4bb008f3adc14c31f5
Mircea Radu <radumircea88@gmail.com>
2021-11-09 04:47:58 +0200

    combineReducers initial explanation

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,6 +1,12 @@
 import { combineReducers } from 'redux';
 import postReducer from './postReducer';
 
+/// https://redux.js.org/api/combinereducers
+/// You can control state key names by using different keys for the reducers in the passed object.
+/// For example, you may call combineReducers({ todos: myTodosReducer, counter: myCounterReducer }) 
+/// so that state shape to be { todos, counter }
+/// up until now, seems that reducers define state shape
+
 export default combineReducers({
   posts: postReducer
 });
