1ab0978e2c31de31c02579178523fc4f663f8639
Mircea Radu <radumircea88@gmail.com>
2021-11-09 04:30:11 +0200

    Update Posts.js

--- src/components/Posts.js
+++ src/components/Posts.js
@@ -41,4 +41,13 @@ const mapStateToProps = state => ({
   newPost: state.posts.item
 });
 
+
+/// connecting a Component to the store
+/// connect() returns a Component that is connected to the Redux store (here named C.)
+/// C. is based on `Posts` component  
+/// (or C. is the `Posts` comp. but with extra capabilities; either way C. is a React comp. that is connected to the Redux Store)
+/// this new C. element that is exported will have the capability to dispatch actions to the store
+/// [dispatch = an action that is sent to the dispatcher (smth. that processes actions and sends it to the root reducer)]
+/// 
+
 export default connect(mapStateToProps, { fetchPosts })(Posts);
