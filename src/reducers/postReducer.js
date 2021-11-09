4a1ef0d5aa50f7172dd263a1c154a44077b6d2cc
Mircea Radu <radumircea88@gmail.com>
2021-11-09 04:59:32 +0200

    rules for defining reducers

--- src/reducers/postReducer.js
+++ src/reducers/postReducer.js
@@ -5,6 +5,20 @@ const initialState = {
   item: {}
 };
 
+
+/// Rules for defining reducers (https://redux.js.org/api/combinereducers)
+/// Any reducer passed to combineReducers must satisfy these rules:
+/// --------------------------------------------------------------------------------------------------------------------
+/// 1. For any action that is not recognized, it must return the state given to it as the first argument.
+/// --------------------------------------------------------------------------------------------------------------------
+/// 2. It must never return undefined. It is too easy to do this by mistake via an early return statement, 
+///    so combineReducers throws if you do that instead of letting the error manifest itself somewhere else.
+/// --------------------------------------------------------------------------------------------------------------------
+/// 3. If the state given to it is undefined, it must return the initial state for this specific reducer.
+///    According to the previous rule, the initial state must not be undefined either.It is handy to specify
+///    it with ES6 optional arguments syntax, but you can also explicitly check the first argument for being undefined.
+/// --------------------------------------------------------------------------------------------------------------------
+
 export default function(state = initialState, action) {
   switch (action.type) {
     case FETCH_POSTS:
